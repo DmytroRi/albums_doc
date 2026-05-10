cc122e68dc6a55254ada953d9af69f33a788afd2
Dmytro Riabchuk <129224584+DmytroRi@users.noreply.github.com>
2026-05-10 14:51:54 +0200

    Fix frontend bootstrap import and add Flutter web scaffold files

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:albums_frontend/app/albums_app.dart';
 import 'package:albums_frontend/features/albums/bloc/albums_bloc.dart';
+import 'package:albums_frontend/features/albums/bloc/albums_event.dart';
 import 'package:albums_frontend/features/albums/repositories/albums_repository.dart';
 import 'package:albums_frontend/generated_api/lib/api.dart';
 import 'package:flutter/material.dart';
